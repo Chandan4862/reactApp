cb37c1eb8f2a0577ea9dca3d3785fbaacbe1cf86
chandan ojha <cojha@zeidlerlegalservices.com>
2023-04-29 10:39:36 +0530

    filter

--- src/components/HelloWorld.js
+++ src/components/HelloWorld.js
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
-const HelloWorld = ()=>{
+const HelloWorld = () => {
+  const filters = ["chandan", "ojha", "time", "imp"];
+  const [search, setSearch] = useState("");
+  return (
+    <div>
+      <input
+        onChange={(e) => {
+          setSearch(e.target.value);
+        }}
+      ></input>
+      <div
+        className="list"
+        style={{ display: "flex", gap: 10, listStyle: "none" }}
+      >
+        {filters
+          .filter((x) => x.includes(search))
+          .map((x) => (
+            <div className="border">{x}</div>
+          ))}
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div>
-            Hello World!
-        </div>
-    )
-}
-
-export default HelloWorld
+export default HelloWorld;
